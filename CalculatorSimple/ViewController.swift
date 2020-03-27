023fddebe39410712eb8fb30ebd7290eced5fe9d
Yunus MAKAN <makanyunus@gmail.com>
2020-03-27 16:07:53 +0300

    Initial Commit

---
+++ CalculatorSimple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorSimple
+//
+//  Created by MAKAN on 27.03.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
